88988229908b1a6ea5b4268b2adc56460d51a603
Kenny Baskett <kbaskett248@gmail.com>
2018-05-15 22:18:07 -0400

    Previous parsing wasn't right.
    The previous commit didn't match json spec. This new version
    runs json.loads on the string, then validates that the value
    matches the data_spec.

--- json_datatypes/__init__.py
+++ json_datatypes/__init__.py
@@ -5,9 +5,49 @@ __author__ = 'Kenny Baskett <kbaskett248@gmail.com>'
 __all__ = []
 
 
+from functools import partial
 import json
+from typing import (
+	Any,
+	TypeVar
+)
+
+T = TypeVar('T')
+
+
+def default_converter(type_: T, value: Any) -> T:
+	"""Generic validator that ensures value is of the specified type."""
+	if isinstance(value, bool):
+		if type_ is not bool:
+			raise ValueError()
+		else:
+			return value
+	if isinstance(value, type_):
+		return value
+	else:
+		raise ValueError()
+
+
+def float_converter(value: Any) -> float:
+	"""Validator that ensures value is a float."""
+	if isinstance(value, bool):
+		raise ValueError()
+	if isinstance(value, float):
+		return value
+	elif isinstance(value, int):
+		return float(value)
+	else:
+		raise ValueError()
+
+
+CONVERTERS = {
+	str: partial(default_converter, str),
+	int: partial(default_converter, int),
+	float: float_converter,
+	bool: partial(default_converter, bool)
+}
 
 
 def parse_string(data_spec: type, json_: str):
     """Parse a string according to the data_spec provided."""
-    return data_spec(json_)
+    return CONVERTERS[data_spec](json.loads(json_))
--- tests/test_parsing.py
+++ tests/test_parsing.py
@@ -11,11 +11,11 @@ from json_datatypes import parse_string
 
 
 @pytest.mark.parametrize('data_spec, json_, value', [
-    (str, 'hello', 'hello'),
-    (str, '5', '5'),
-    (str, '1.5', '1.5'),
-    (str, 'true', 'true'),
-    (str, 'false', 'false'),
+    (str, '"hello"', 'hello'),
+    (str, '"5"', '5'),
+    (str, '"1.5"', '1.5'),
+    (str, '"true"', 'true'),
+    (str, '"false"', 'false'),
     (int, '1', 1),
     (float, '1', 1.0),
     (float, '1.5', 1.5),
@@ -30,15 +30,34 @@ def test_parse_basic_value(data_spec: type, json_: str, value: Any) -> None:
 
 
 @pytest.mark.parametrize('data_spec, json_', [
+    (str, 'hello'),
+    (str, '1'),
+    (str, '1.5'),
+    (str, 'true'),
+    (str, 'false'),
     (int, 'hello'),
-    (float, 'hello'),
-    (bool, 'hello'),
+    (int, '"hello"'),
+    (int, '"1"'),
+    (int, '"1.5"'),
     (int, '1.5'),
-    (bool, '1.5'),
+    (int, 'true'),
+    (int, 'false'),
+    (float, 'hello'),
+    (float, '"hello"'),
+    (float, '"1"'),
+    (float, '"1.5"'),
     (float, 'true'),
-    (float, 'false')
+    (float, 'false'),
+    (bool, 'hello'),
+    (bool, '"hello"'),
+    (bool, '"true"'),
+    (bool, '"false"'),
+    (bool, '"1"'),
+    (bool, '"1.5"'),
+    (bool, '1'),
+    (bool, '1.5'),
 ])
 def test_throws_error_on_incompatible_basic_values(
         data_spec: type, json_: str) -> None:
     with pytest.raises(ValueError):
-        parse_string(data_spec, json_)
+        print(parse_string(data_spec, json_))
